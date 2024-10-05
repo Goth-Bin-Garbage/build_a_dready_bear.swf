2d9607c4ee222b57cd7456024ca18972892b4397
orbitaldot <orbitaldotogg@gmail.com>
2024-10-06 00:07:10 +0200

    Update main.gd

--- main.gd
+++ main.gd
@@ -5,7 +5,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_station("FabricStation")
+	# load_station("FabricStation")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

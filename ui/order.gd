48ad2eeaf36ac71f7a63dc3c3b2d228ed63cb66a
orbitaldot <orbitaldotogg@gmail.com>
2024-10-06 19:27:52 +0200

    Add orders to UI

--- ui/order.gd
+++ ui/order.gd
@@ -5,10 +5,17 @@ extends Node
 @onready var head_shape := Global.get_random_doll_headshape()
 @onready var eyes := Global.get_random_doll_eyes()
 
+@onready var eyes_label := $EyesLabel
+@onready var pattern_label := $PatternLabel
+@onready var color_label := $ColorLabel
+@onready var head_label := $HeadLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pattern_label.text = "pattern: " + str(pattern)
+	color_label.text = "color: " + str(color)
+	eyes_label.text = "eyes: " + str(eyes)
+	head_label.text = "head: " + str(head_shape)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

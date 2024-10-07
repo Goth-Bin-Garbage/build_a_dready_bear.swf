4dcfae72c1fb491c95d0a30a57f8e91c2709461d
orbitaldot <orbitaldotogg@gmail.com>
2024-10-07 23:03:09 +0200

    fix eye seed packets

--- stations/station_elements/eye_seed_packet.gd
+++ stations/station_elements/eye_seed_packet.gd
@@ -15,17 +15,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("left_mouse_click"):
+	if Input.is_action_just_pressed("left_mouse_click") and mouse_on:
 		offset = get_global_mouse_position() - global_position
 		self.rotate(-PI / 4)
 		
-		if Input.is_action_pressed("left_mouse_click"):
-			global_position = get_global_mouse_position() - offset
-		
-		elif Input.is_action_just_released("left_mouse_click"):
-			var tween = get_tree().create_tween()
-			self.rotate(0)
-			tween.tween_property(self, "global_position",home_pos,0.2).set_ease(Tween.EASE_OUT)
+	if Input.is_action_pressed("left_mouse_click") and mouse_on:
+		global_position = get_global_mouse_position() - offset
+	
+	if Input.is_action_just_released("left_mouse_click"):
+		var tween = get_tree().create_tween()
+		self.rotation = 0
+		tween.tween_property(self, "global_position",home_pos,0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered():
 	mouse_on = true
@@ -38,5 +38,6 @@ func _on_drop_collision_area_entered(area):
 	var eye_plant_instance := eye_plant_scene.instantiate()
 	eye_plant_instance.plant_type = eye_type
 	eye_plant_instance.global_position = area.get_parent().global_position
-	eye_plant_instance.name = "Plant_" + area.get_partent().name
-	get_parent().get_parent().get_node("Plants").add_child(eye_plant_instance)
+	eye_plant_instance.name = "Plant_" + area.get_parent().name
+	print(eye_plant_instance)
+	get_parent().get_node("Plants").add_child(eye_plant_instance)

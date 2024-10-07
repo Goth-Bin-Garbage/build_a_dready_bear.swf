4d2ea6287ca293fb9e9aec7f5c9f358d4db9eac4
cnj3565 <cjensen6050@gmail.com>
2024-10-06 23:43:59 -0400

    Head Station set up visually

--- stations/head_station.gd
+++ stations/head_station.gd
@@ -2,10 +2,6 @@ extends Node2D
 
 # do dials[i].head_type for their value
 @onready var dials : Array = $Dials.get_children()
-var selected_heads : Array = []
-
-@onready var mouse_on_master : bool = false
-@onready var clickable_master : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,19 +9,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if mouse_on_master && clickable_master:
-		if Input.is_action_just_pressed("left_mouse_click"):
-			clickable_master = false
-			$MasterDial.rotate(PI / 2)
-			$MasterDialTimer.start()
-			# send selected_heads to mixer array
-
-func _on_area_2d_mouse_entered():
-	mouse_on_master = true
-
-func _on_area_2d_mouse_exited():
-	mouse_on_master = false
-
-func _on_master_dial_timer_timeout():
-	$MasterDial.rotation = 0.0
-	clickable = true
+	pass
